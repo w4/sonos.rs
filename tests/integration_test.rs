6c27dae46da7ce208fc2687c67412427e5fc5645
Jordan Doyle <jordan@doyle.wf>
2017-11-27 02:53:05 +0000

    Rename crate to sonos

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,14 +1,14 @@
-extern crate soncon;
+extern crate sonos;
 
 #[test]
 fn can_discover_devices() {
-    let devices = soncon::discover().unwrap();
+    let devices = sonos::discover().unwrap();
     assert!(devices.len() > 0, "No devices discovered");
 }
 
 #[test]
 fn volume() {
-    let device = &soncon::discover().unwrap()[0];
+    let device = &sonos::discover().unwrap()[0];
     device.set_volume(2).expect("Failed to get volume");
     assert_eq!(
         device.volume().expect("Failed to get volume"),
@@ -19,7 +19,7 @@ fn volume() {
 
 #[test]
 fn muted() {
-    let device = &soncon::discover().unwrap()[0];
+    let device = &sonos::discover().unwrap()[0];
     device.mute().expect("Couldn't mute player");
     assert_eq!(
         device.muted().expect("Failed to get current mute status"),
@@ -34,39 +34,39 @@ fn muted() {
 
 #[test]
 fn playback_state() {
-    let device = &soncon::discover().unwrap()[0];
+    let device = &sonos::discover().unwrap()[0];
 
     device.play().expect("Couldn't play track");
     assert!(match device.transport_state().unwrap() {
-        soncon::TransportState::Playing => true,
-        soncon::TransportState::Transitioning => true,
+        sonos::TransportState::Playing => true,
+        sonos::TransportState::Transitioning => true,
         _ => false,
     });
 
     device.pause().expect("Couldn't pause track");
     assert!(match device.transport_state().unwrap() {
-        soncon::TransportState::PausedPlayback => true,
-        soncon::TransportState::Transitioning => true,
+        sonos::TransportState::PausedPlayback => true,
+        sonos::TransportState::Transitioning => true,
         _ => false,
     });
 
     device.stop().expect("Couldn't stop track");
     assert!(match device.transport_state().unwrap() {
-        soncon::TransportState::Stopped => true,
-        soncon::TransportState::Transitioning => true,
+        sonos::TransportState::Stopped => true,
+        sonos::TransportState::Transitioning => true,
         _ => false,
     });
 }
 
 #[test]
 fn track_info() {
-    let device = &soncon::discover().unwrap()[0];
-    println!("{:#?}", device.track().expect("Failed to get track info"));
+    let device = &sonos::discover().unwrap()[0];
+    device.track().expect("Failed to get track info");
 }
 
 #[test]
 fn play() {
-    let device = &soncon::discover().unwrap()[0];
+    let device = &sonos::discover().unwrap()[0];
     device.play().expect("Failed to play");
     device.pause().expect("Failed to pause");
 }
@@ -74,7 +74,7 @@ fn play() {
 #[test]
 #[should_panic]
 fn fail_on_set_invalid_volume() {
-    soncon::discover().unwrap()[0]
+    sonos::discover().unwrap()[0]
         .set_volume(101)
         .expect_err("Didn't fail on invalid volume");
 }
